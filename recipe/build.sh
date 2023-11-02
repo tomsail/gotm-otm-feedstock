3c0788916f48d43904b387f549fc6d3d66fe28de
tomsail <saillour.thomas@gmail.com>
2023-11-02 09:06:56 +0100

    initial commit

---
+++ recipe/build.sh
@@ -0,0 +1,11 @@
+
+git clone --recursive https://github.com/gotm-model/code.git
+cd code
+git checkout $VERSION
+git submodule update --init --recursive
+
+mkdir build
+cd build
+cmake .. -DGOTM_USE_NetCDF:BOOL=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX
+make
+make install
